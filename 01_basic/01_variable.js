0790fcae5bb227076f9bc1a7601aa313e861b58c
nachiket10700 <nachiketkondawar98@gmail.com>
2024-08-13 12:44:14 +0000

    data_types

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,14 @@
+const accountId = 45654
+let accountEmail = "nachiketkondawar98@gmail.com"
+var accountPassword = "5685"
+accountCity = "latur"
+
+accountEmail ="navsudidh@gmail.com"
+accountPassword="556465"
+accountCity= "bangluru"
+
+// console.log(accountId);
+
+// never use var 
+
+console.table([accountEmail, accountId, accountPassword, accountCity]);
---
+++ 01_basic/02_datatype.js
@@ -0,0 +1,11 @@
+let name = " nachiket" // string
+let age = 18   //integer
+let isCoolguy = true  //boolean
+let state = null
+
+//null=> standalone value
+//symbol =>unique
+
+//object
+
+console.log(typeof"nachiket");
