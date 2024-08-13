dbbf19b20d9f13ef7eb512e4d0c8621a50271eb7
nachiket10700 <nachiketkondawar98@gmail.com>
2024-08-13 13:03:02 +0000

    03_convestionOperation.js

---
+++ 01_basic/03_convestionOperation.js
@@ -0,0 +1,18 @@
+let isLoggedIn = "nachiket"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+// console.log(booleanIsLoggedIn);
+
+// 1=>true
+// ""=>false
+//string =>true
+
+
+
+let someNumber = 66
+
+let stringNumber = String(someNumber)
+
+console.log(stringNumber)
+console.log(typeof"stringNumber")
